4fbf2b6ff198cf4fdc421c4ebd6fa857a730ab27
Philipp Seerainer <50533219+seerainer@users.noreply.github.com>
2024-03-13 01:18:53 +0100

    Update Crypto.java

--- src/main/java/io/github/secpwdman/io/crypto/Crypto.java
+++ src/main/java/io/github/secpwdman/io/crypto/Crypto.java
@@ -2,7 +2,7 @@
  * Secure Password Manager
  * Copyright (C) 2024  Philipp Seerainer
  * philipp@seerainer.com
- * http://www.seerainer.com/
+ * https://www.seerainer.com/
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
