a18a2965cd46c6af3dc2792e585b91e17d3ece3e
Philipp <50533219+seerainer@users.noreply.github.com>
2025-08-09 15:57:51 +0200

    Update build.cmd

--- JNI/build.cmd
+++ JNI/build.cmd
@@ -2,6 +2,7 @@
 
 call "%PROGRAMFILES%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-@REM gcc -c -Os -flto -o window_affinity.o window_affinity.c
+@REM gcc -c -Os -o window_affinity.o window_affinity.c
 
 cl /c /O1 window_affinity.c
+
